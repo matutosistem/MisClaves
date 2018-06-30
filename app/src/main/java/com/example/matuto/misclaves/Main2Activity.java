e414416055d03b1d1c614f81499166b648842307
matuto <matutosistem@gmail.com>
2018-06-29 20:08:47 -0400

    2906

--- app/src/main/java/com/example/matuto/misclaves/Main2Activity.java
+++ app/src/main/java/com/example/matuto/misclaves/Main2Activity.java
@@ -1,13 +1,53 @@
 package com.example.matuto.misclaves;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.OutputStreamWriter;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private TextView tvRut;
+    private ImageButton ibtAgregar;
+    private ListView LstCuentas;
+    private String usuario;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        tvRut = (TextView) findViewById(R.id.tvRut);
+        ibtAgregar=(ImageButton) findViewById(R.id.ibtAgregar);
+        LstCuentas=(ListView) findViewById(R.id.Lstcuentas);
+
+
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle != null) {
+
+
+            usuario=bundle.get("usuario").toString();
+            tvRut.setText(bundle.get("usuario").toString());
+        }
+
+
+        try {
+        OutputStreamWriter osw = new OutputStreamWriter(
+                openFileOutput(usuario+".txt", Activity.MODE_PRIVATE));
+
+        } catch (Exception ex) {
+
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/matuto/misclaves/MainActivity.java
+++ app/src/main/java/com/example/matuto/misclaves/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.matuto.misclaves;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,6 +24,47 @@ public class MainActivity extends AppCompatActivity {
         etUsuario = (EditText) findViewById(R.id.etUsuario);
         etClave = (EditText) findViewById(R.id.etClave);
         btIngresar = (Button) findViewById(R.id.btIngresar);
+
+
+        btIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guardarUsuario();
+            }
+        });
     }
+
+
+
+    private void guardarUsuario(){
+
+        SharedPreferences pref=getSharedPreferences("usuarios", Context.MODE_PRIVATE);
+
+
+        if(pref.getString(etUsuario.getText().toString(),"No existe").equals("No existe")){
+
+            SharedPreferences.Editor edit=pref.edit();
+            edit.putString(etUsuario.getText().toString(), etClave.getText().toString());
+            edit.commit();
+
+        }else{
+            if(pref.getString(etUsuario.getText().toString(), "Clave incorrecta").equals(etClave.getText().toString())){
+
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                intent.putExtra("usuario", etUsuario.getText().toString());
+                startActivity(intent);
+            }else{
+                etClave.setError("Clave Incorrecta");
+            }
+
+        }
+
+
+
+
+
+
+    }
+
 }
 
